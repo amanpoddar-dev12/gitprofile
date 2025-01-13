c12c3ae71701227433713dc9214259ff12c7805f
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-01-14 00:07:27 +0530

    Update footer

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -231,11 +231,11 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/amanpoddar-dev12/gitprofile"
+  footer: `Made by <a 
+      class="text-primary" href="https://github.com/amanpoddar-dev12/"
       target="_blank"
       rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+    >Aman Poddar</a> ❤️`,
 
   enablePWA: true,
 };
